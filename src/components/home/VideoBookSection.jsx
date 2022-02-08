fb18a261894858ae3d08221dc603801766841683
Tiberius Mairura <tiberiusmairura90@gmail.com>
2022-02-08 05:41:31 +0300

    Changes to home page

--- src/components/home/VideoBookSection.jsx
+++ src/components/home/VideoBookSection.jsx
@@ -4,31 +4,32 @@ import book5 from '../../assets/book5.png';
 
 function VideoBookSection() {
   return (
-    <div className="m-3">
+    <div className="p-3">
       {/* Stack the columns on mobile by making one full-width and the other half-width */}
-      <Row>
-        <Col xs={12} md={9} className="">
-          <Card>
-            <iframe
-              height="323"
-              src="https://www.youtube.com/embed/f7iAza3DMRg"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </Card>
+      <Row className="m-0 p-0">
+        <Col xs={12} md={7} className="p-0 m-0">
+          {/* <Card className="p-0 m-0 mb-xs-5"> */}
+          <iframe
+            height="323"
+            width="570"
+            src="https://www.youtube.com/embed/f7iAza3DMRg"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          {/* </Card> */}
         </Col>
-        <Col xs={4} md={2} className="mt-xs-5 mt-md-0 book">
-          <Card>
-            <Card.Img
-              variant="top"
-              src={book5}
-              style={{ width: 14.3 + 'rem' }}
-              fluid="true"
-              id={book5}
-            />
-          </Card>
+        <Col xs={4} md={5} className="mt-sm-4 mt-md-0 book">
+          {/* <Card className="p-0 m-0-"> */}
+          <Card.Img
+            variant="top"
+            src={book5}
+            style={{ width: 14.3 + 'rem' }}
+            fluid="true"
+            id={book5}
+          />
+          {/* </Card> */}
         </Col>
       </Row>
     </div>
--- src/components/utilities/HeroSection.jsx
+++ src/components/utilities/HeroSection.jsx
@@ -6,23 +6,23 @@ import item1 from '../../assets/item-1.png';
 function HeroSection() {
   return (
     <div className="bg-light text-dark">
-      <Carousel>
-        <Carousel.Item>
+      <Carousel style={{ height: 65 + `vh` }}>
+        <Carousel.Item className="hero-height">
           <img className="d-block w-100 " src={item1} alt="First slide" />
-          <Carousel.Caption className="text-dark">
+          <Carousel.Caption className="text-center">
             <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="hero-height">
           <img className="d-block w-100  " src={item1} alt="Second slide" />
 
-          <Carousel.Caption className="text-dark">
+          <Carousel.Caption className="text-dark mt-0 p-0">
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item className="hero-height">
           <img className="d-block w-100 " src={item1} alt="Third slide" />
 
           <Carousel.Caption className="text-dark">
