a93db84b58316769d98e1cd69764b3542897ee2f
Tiberius Mairura <tiberiusmairura90@gmail.com>
2022-02-08 14:24:22 +0300

    fix layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import HeroSection from './utilities//HeroSection';
 import AboutInfo from './about/AboutInfo';
-import BookCenter from './about/BookCenter';
-import Daughters from './about/Daughters';
 
 function About() {
   return (
     <div>
       <HeroSection />
       <AboutInfo />
-      <BookCenter />
-      <Daughters />
     </div>
   );
 }
