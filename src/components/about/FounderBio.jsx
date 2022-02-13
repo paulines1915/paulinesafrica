f3cba2093fc678462a57a74c0621479a1480931e
Tiberius Mairura <tiberiusmairura90@gmail.com>
2022-02-13 17:16:03 +0300

    modify founders page

---
+++ src/components/about/FounderBio.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Card, Col } from 'react-bootstrap';
+
+function FounderBio() {
+  return (
+    <Col>
+      <Card className="border-0">
+        <Card.Body>
+          <Card.Text>
+            Like Paul, who declared: "The love of Christ urges us on" (2Cor
+            5:14), at the dawn of the 20th century Blessed Alberione felt
+            interiorly moved by an appeal of love from Jesus in the Eucharist,
+            who yearns to draw all people to himself and who prompted him to
+            begin a new apostolate and a new religious family. Thus the young
+            priest set out on an incredible adventure that, over the arch of
+            many decades, led him to initiate innumerable projects. He started
+            in 1914 with a small group of boys and the command to publish Alba's
+            diocesan newspaper, and by the end of his life he had founded five
+            religious congregations, four secular institutes, numerous Catholic
+            publishing houses throughout the world, magazines, a network of
+            media centres that he hoped would serve as sources of light for all
+            who visited them. His original interest in the medium of the press
+            expanded as he realised that the Church of today must be present in
+            the cinema, in television, in the world of music and in every field
+            of human communication furnished by progress. He did not live to see
+            the development of the Internet, but all the languages and
+            instruments of modern communication attracted his interest because
+            he saw them as ways of responding to the social and pastoral
+            problems of his time. Participating in Vatican Council II, he had
+            the satisfaction of seeing the Council documents acknowledge many of
+            the intuitions he had laboured long and hard to concretise.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
+
+export default FounderBio;
--- src/components/about/Founders.jsx
+++ src/components/about/Founders.jsx
@@ -1,28 +1,58 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { Row, Col, Tabs, Tab, Container, Card } from 'react-bootstrap';
 import HeroSection from '../utilities/HeroSection';
+import FounderBio from './FounderBio';
+import FounderPrayer from './FounderPrayer';
 import FoundersAvatar from './FoundersAvatar';
 
+import james from '../../assets/james-cover.jpg';
+import FounderContact from './FounderContact';
+
 function Founders() {
+  const [key, setKey] = useState('name');
   return (
-    <div>
+    <>
       <HeroSection />
-      <Card className="border-0 p-3">
-        <Card.Title className="text-maroon">Blessed James Alberione</Card.Title>
-        <Card.Body style={{ maxWidth: 731 + `px`, margin: 0 + ` auto` }}>
-          <Card.Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            lobortis nisi est, quis viverra leo placerat a. Donec massa nibh,
-            cursus finibus dui eget, accumsan ullamcorper purus. Duis sagittis
-            et tellus blandit viverra. Sed libero enim, imperdiet ut condimentum
-            in, mattis ac risus. Curabitur non feugiat nunc, bibendum congue
-            ipsum. In eros diam, maximus ac
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Button className="mb-3 px-5 bg-main">Read More</Button>
-      <FoundersAvatar />
-    </div>
+      <Container>
+        <Row xs={1} md={2} className="pt-5 px-4 text-start">
+          <Col md={5} className="p-3">
+            <h4>Blessed James Alberione Founder of the Pauline Family</h4>
+            <img src={james} alt="" style={{ maxWidth: 100 + `%` }} />
+          </Col>
+          <Col md={7}>
+            <Tabs
+              id="controlled-tab-example"
+              activeKey={key}
+              onSelect={(k) => setKey(k)}
+              className="mb-3"
+            >
+              <Tab eventKey="name" title="Blessed James Alberione">
+                <FounderBio />
+              </Tab>
+              <Tab eventKey="prayer" title="Prayer">
+                <FounderPrayer />
+              </Tab>
+              <Tab eventKey="contact" title="Contact Information">
+                <FounderContact />
+              </Tab>
+            </Tabs>
+          </Col>
+
+          <Col md={12} className="text-center my-3 ">
+            <Card className="py-5 px-3 bg-main text-white rounded-0">
+              <Card.Text>
+                Blessed James Alberione sa the mission of communication not as a
+                “business,” but as preaching, as an exercise of the ministry of
+                salvation, in which we offer Jesus as a gift to others.
+              </Card.Text>
+            </Card>
+          </Col>
+        </Row>
+
+        <FoundersAvatar />
+      </Container>
+    </>
   );
 }
 
