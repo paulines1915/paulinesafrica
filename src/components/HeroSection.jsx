9a447c8d8b6d5b1b02716243745e090504fa4368
Tiberius Mairura <tiberiusmairura90@gmail.com>
2022-02-17 19:54:31 +0300

    add assets

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 
 import { Carousel } from 'react-bootstrap';
-import item1 from '../assets/item-1.png';
+
+import banner1 from '../assets/banner-1.png';
+import banner2 from '../assets/banner-2.png';
+import banner3 from '../assets/banner-3.png';
+import banner4 from '../assets/banner-4.png';
+import banner5 from '../assets/banner-5.png';
 
 function HeroSection() {
   return (
     <div className="bg-light text-dark">
       <Carousel>
         <Carousel.Item>
-          <img className="d-block w-100 " src={item1} alt="First slide" />
+        
+          <img className="d-block w-100" src={banner1} alt="First slide" />
           <Carousel.Caption className="text-dark">
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100  " src={item1} alt="Second slide" />
+          <img className="d-block w-100" src={banner2} alt="Second slide" />
 
           <Carousel.Caption className="text-dark">
             <h3>Second slide label</h3>
@@ -23,7 +29,7 @@ function HeroSection() {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100 " src={item1} alt="Third slide" />
+          <img className="d-block w-100" src={banner3} alt="Third slide" />
 
           <Carousel.Caption className="text-dark">
             <h3>Third slide label</h3>
