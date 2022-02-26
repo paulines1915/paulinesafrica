74bb5ed5007fb8beeac5d4c65edca592da3448e0
Tiberius Mairura <tiberiusmairura90@gmail.com>
2022-02-27 01:05:39 +0300

    another trial

--- backendapi/urls.py
+++ backendapi/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import template
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('api.api.urls'))
+    path('api/', include('api.api.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
